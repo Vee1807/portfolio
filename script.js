f2e6b7b4c534731806c2280f33be6990e8f1e059
vanykardjikian <125376159+vanykardjikian@users.noreply.github.com>
2024-05-15 16:06:00 +0400

    Add scroll animations

--- script.js
+++ script.js
@@ -24,3 +24,18 @@ window.onscroll = () => {
         }
     })
 }
+
+
+// Scroll animations
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show-animation')
+        } else {
+            entry.target.classList.remove('show-animation')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden-elem')
+hiddenElements.forEach(elem => observer.observe(elem))
