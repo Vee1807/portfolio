f6e3f23a7b7418f23a12f102b687e1843a6d4c87
vanykardjikian <125376159+vanykardjikian@users.noreply.github.com>
2024-05-16 00:55:43 +0400

    Add gradient to 'Software Developer' and animate it

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ const getIcon = (theme) => {
 }
 
 const setTheme = (theme) => {
-    themeBtn.firstElementChild.classList = getIcon(theme)
-    localStorage.setItem("theme", theme)
+	themeBtn.firstElementChild.classList = getIcon(theme)
+	localStorage.setItem("theme", theme)
 }
 
 window.onload = setTheme(document.documentElement.getAttribute('data-theme'))
 
 const themeToggle = () => {
-    let theme = document.documentElement.getAttribute('data-theme')
-    if (theme === "dark") {
+	let theme = document.documentElement.getAttribute('data-theme')
+	if (theme === "dark") {
 		theme = "light"
 	} else {
 		theme = "dark"
 	}
-    document.documentElement.setAttribute('data-theme', theme)
-    setTheme(theme)
+	document.documentElement.setAttribute('data-theme', theme)
+	setTheme(theme)
 }
 
 themeBtn.addEventListener('click', themeToggle)
@@ -38,36 +38,49 @@ const sections = document.querySelectorAll('section')
 const menuIcn = document.querySelector('#menu-icon')
 const navbar = document.querySelector('.navbar')
 menuIcn.addEventListener('click', () => {
-    navbar.classList.toggle('active')
+	navbar.classList.toggle('active')
 })
 
 
 window.onscroll = () => {
-    navbar.classList.remove('active')
-    sections.forEach(section => {
-        const sectionY = window.scrollY + section.getBoundingClientRect().top
-        const id = section.id
-        if (sectionY <= window.scrollY + 50 && sectionY + section.scrollHeight >= window.scrollY) {
-            navbarLinks.forEach(link => {
-                link.classList.remove('current')
-            })
-            const currentLink = document.querySelector(`.navbar a[href*='${id}']`)
-            currentLink.classList.add('current')
-        }
-    })
+	navbar.classList.remove('active')
+	sections.forEach(section => {
+		const sectionY = window.scrollY + section.getBoundingClientRect().top
+		const id = section.id
+		if (sectionY <= window.scrollY + 50 && sectionY + section.scrollHeight >= window.scrollY) {
+			navbarLinks.forEach(link => {
+				link.classList.remove('current')
+			})
+			const currentLink = document.querySelector(`.navbar a[href*='${id}']`)
+			currentLink.classList.add('current')
+		}
+	})
 }
 
 
 // Scroll animations
 const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show-animation')
-        } else {
-            entry.target.classList.remove('show-animation')
-        }
-    })
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show-animation')
+		} else {
+			entry.target.classList.remove('show-animation')
+		}
+	})
 })
 
 const hiddenElements = document.querySelectorAll('.hidden-elem')
 hiddenElements.forEach(elem => observer.observe(elem))
+
+
+// "Software Developer" hover gradient
+const specialtyTitle = document.getElementById('specialty-title');
+
+specialtyTitle.addEventListener('mousemove', e => {
+	const rect = e.target.getBoundingClientRect()
+
+	const x = e.clientX - rect.left
+	const y = e.clientY - rect.top
+
+	specialtyTitle.style.backgroundPosition = `${x}px ${y}px`;
+})
